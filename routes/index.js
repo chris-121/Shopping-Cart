c44ea34654df01ccf74e7c9b6c263a408363baa8
Chris Jacob <chrisjacob876@gmail.com>
2021-09-15 22:42:56 +0530

    layouts created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let product=[
+  {
+    name:'Iphone XR', 
+    category:'Mobile', 
+    price:75000,
+    description:'the brand new Iphone XR ',
+    Image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-white-select-201809?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1551226036668"
+  
+  },
+  {
+    name:'Iphone XR' ,
+    category:'Mobile' ,
+    price:75000,
+    description:'the brand new Iphone XR',
+    Image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-white-select-201809?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1551226036668" 
+  
+  },
+  {
+    name:'Iphone XR' ,
+    category:'Mobile' ,
+    price:75000,
+    description:'the brand new Iphone XR' ,
+    Image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-white-select-201809?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1551226036668"
+  
+  },
+  {
+    name:'Iphone XR' ,
+    category:'Mobile' ,
+    price:75000,
+    description:'the brand new Iphone XR' ,
+    Image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-white-select-201809?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1551226036668"
+  
+  },
+
+]
+  res.render('index', { title: 'Express',product,admin:true});
 });
 
 module.exports = router;
